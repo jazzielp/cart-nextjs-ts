c57a370c217aa37766ae13f6b5796ce2af5fea6e
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-11 16:24:51 -0600

    Add cart API with GET and POST methods for managing products

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Product } from "@/app/interfaces/interface";
+
+const cart: Product[] = [];
+
+export async function GET() {
+  return NextResponse.json(cart);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const idProduct = Number(body.idProduct);
+  const response = await fetch("http://localhost:3000/api/products");
+  const products: Product[] = await response.json();
+
+  const product = products.find((product) => product.id === idProduct);
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+
+  cart.push(product);
+  return NextResponse.json({
+    message: "Producto agregado al carrito",
+    data: cart,
+  });
+}
