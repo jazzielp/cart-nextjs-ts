4edbc82708eae661c59ee80935259dead37f0af0
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-13 15:52:43 -0600

    Refactor Budget component layout for improved responsiveness and structure

--- src/app/components/Budget.tsx
+++ src/app/components/Budget.tsx
@@ -20,24 +20,26 @@ export function Budget() {
   }
 
   return (
-    <div className="flex flex-row items-center gap-3 w-full max-w-xl">
+    <div className="flex flex-col md:flex-row justify-center md:justify-between md:items-center gap-3 w-full lg:max-w-2xl">
       <p className="text-sm whitespace-nowrap">
         Calcula tu compra en base a tu presupuesto
       </p>
-      <input
-        type="text"
-        name="budget"
-        placeholder="$300"
-        onChange={handleInputChange}
-        value={budget}
-        className="flex-1 rounded-lg border border-gray-300 dark:border-gray-700 bg-white/80 dark:bg-gray-800/70 px-3 py-2 text-sm text-gray-900 dark:text-gray-100 placeholder:text-gray-400 dark:placeholder:text-gray-500 shadow-inner focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition"
-      />
-      <button
-        className="inline-flex items-center justify-center rounded-lg bg-indigo-600 text-white px-4 py-2 text-sm font-medium shadow hover:bg-indigo-500 active:bg-indigo-600/90 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition cursor-pointer"
-        onClick={handleCalculate}
-      >
-        Calcular
-      </button>
+      <div>
+        <input
+          type="text"
+          name="budget"
+          placeholder="$300"
+          onChange={handleInputChange}
+          value={budget}
+          className=":sm:flex-1 rounded-lg border border-gray-300 dark:border-gray-700 bg-white/80 dark:bg-gray-800/70 px-3 py-2 text-sm text-gray-900 dark:text-gray-100 placeholder:text-gray-400 dark:placeholder:text-gray-500 shadow-inner focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition"
+        />
+        <button
+          className="inline-flex items-center justify-center rounded-lg bg-indigo-600 text-white px-4 py-2 text-sm font-medium shadow hover:bg-indigo-500 active:bg-indigo-600/90 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition cursor-pointer ml-4"
+          onClick={handleCalculate}
+        >
+          Calcular
+        </button>
+      </div>
     </div>
   )
 }
