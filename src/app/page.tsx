3151259c62c666d8041464bfd4630d2c3c06bf0d
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-13 00:10:34 -0700

    Update Home component to conditionally render BestCombination or ListProducto based on bestCombination state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { useEffect } from "react";
 import { Cart } from "./components/Cart";
 import { useStore } from "@/app/stores/store";
 import { ListProducto } from "./components/ListProducto";
+import { BestCombination } from "./components/BestCombination";
 
 export default function Home() {
   const isCartOpen = useStore((state) => state.isCartOpen);
   const setProducts = useStore((state) => state.setProducts);
+  const bestCombination = useStore((state) => state.bestCombination);
 
 
   useEffect(() => {
@@ -20,7 +22,9 @@ export default function Home() {
 
   return (
     <>
-      <ListProducto />
+      {
+        bestCombination.length > 0 ? <BestCombination /> : <ListProducto />
+      }
       {isCartOpen && <Cart />}
     </>
   );
