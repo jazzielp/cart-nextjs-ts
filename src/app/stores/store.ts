ee49ecead262b3e1fb109fe0686c73fdd35d760f
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-12 22:57:49 -0700

    Add Zustand store properties and methods for cart and product management

--- src/app/stores/store.ts
+++ src/app/stores/store.ts
@@ -1,11 +1,24 @@
 import { create } from "zustand";
+import { Product } from "../interfaces/interface";
 
 interface Store {
   isCartOpen: boolean;
   setIsCartOpen: (isOpen: boolean) => void;
+  cart: Product[];
+  setCart: (products: Product[]) => void;
+  products: Product[];
+  setProducts: (products: Product[]) => void;
+  bestCombination: Product[];
+  setBestCombination: (products: Product[]) => void;
 }
 
 export const useStore = create<Store>((set) => ({
   isCartOpen: false,
   setIsCartOpen: (isOpen) => set({ isCartOpen: isOpen }),
+  cart: [],
+  setCart: (products) => set({ cart: products }),
+  products: [],
+  setProducts: (products) => set({ products: products }),
+  bestCombination: [],
+  setBestCombination: (products) => set({ bestCombination: products }),
 }));
