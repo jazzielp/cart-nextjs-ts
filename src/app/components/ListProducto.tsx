bfcbdaee7aaad001cd4b72c668f8e216df18d6c9
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-12 22:56:52 -0700

    Add ListProducto component to display products using Zustand store

---
+++ src/app/components/ListProducto.tsx
@@ -0,0 +1,22 @@
+import { Budget } from "./Budget";
+import { useStore } from "@/app/stores/store";
+import { ProductCard } from "./ProductCard";
+
+export function ListProducto() {
+  const products = useStore((state) => state.products);
+  return (
+    <main className="">
+      <div className="flex justify-between max-w-7xl m-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10">
+        <div>
+          <h2 className="text-4xl">Productos</h2>
+        </div>
+        <Budget />
+      </div>
+      <div className="max-w-7xl m-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10 grid gap-8 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+        {products && products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+  )
+}
