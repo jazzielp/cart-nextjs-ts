2f3bd4b2754719b9ba147db822bec470f2365274
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-12 18:27:12 -0600

    Refactor Home component layout to integrate Budget component and streamline product display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { ProductCard } from "@/app/components/ProductCard";
 import { Product } from "@/app/interfaces/interface";
-import { Cart } from "./components/Cart";
+import { Budget } from "./components/Budget";
 
 export default async function Home() {
   const responseProducts = await fetch("http://localhost:3000/api/products");
@@ -8,26 +8,19 @@ export default async function Home() {
   const responseCart = await fetch("http://localhost:3000/api/cart");
   const cart = await responseCart.json();
   return (
-    <>
-      <header className="w-full px-6 py-4 md:px-10 md:py-6 flex items-center justify-between bg-white/70 dark:bg-slate-900/60 backdrop-blur-sm border-b border-slate-200 dark:border-slate-800 sticky top-0 z-50">
+    <main className="">
+      <div className="flex justify-between max-w-7xl m-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10">
         <div>
-          <h1 className="text-2xl md:text-3xl ml-6 font-extrabold tracking-tight bg-gradient-to-r from-slate-900 to-blue-600 dark:from-white dark:to-blue-400 bg-clip-text text-transparent">
-            Mi tienda
-          </h1>
-        </div>
-      </header>
-      <main className="">
-        <div className="flex justify-between max-w-7xl ml-8 px-4 sm:px-6 lg:px-8 py-6 md:py-10">
           <h2 className="text-4xl">Productos</h2>
         </div>
-        <Cart cart={cart} />
-        <div className="max-w-5xl ml-8 px-4 sm:px-6 lg:px-8 py-6 md:py-10 grid gap-8 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
-          {products && products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
+        <Budget products={products} />
+      </div>
+      <div className="max-w-7xl m-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10 grid gap-8 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+        {products && products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
 
-      </main>
-    </>
+    </main>
   );
 }
