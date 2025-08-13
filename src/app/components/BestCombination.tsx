1a0f30a2ae76c735d9a19227bd2123ccb1abdc28
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-08-13 15:59:24 -0600

    Update grid layout in BestCombination component for improved responsiveness

--- src/app/components/BestCombination.tsx
+++ src/app/components/BestCombination.tsx
@@ -28,7 +28,7 @@ export function BestCombination() {
           </p>
         </div>
       </div>
-      <div className="max-w-7xl m-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10 grid gap-8 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="max-w-7xl m-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10 grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {bestCombination && bestCombination.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
